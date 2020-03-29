b92df350ff144bb8bc2e02166d9d1f6697a3932c
phonght32 <thanhphongho1998@gmail.com>
2020-03-30 00:35:44 +0700

    add make file

---
+++ make/project_dir.mk
@@ -0,0 +1,5 @@
+CMSIS_PATH := stm-idf/drivers/cmsis
+CMSIS_RTOS_PATH := stm-idf/drivers/cmsis_rtos
+STM32F4XX_HAL_PATH := stm-idf/drivers/stm32f4xx_hal_driver
+RTOS_PATH := stm-idf/components/freertos
+IDF_COMPONENT_PATH := stm-idf/components
