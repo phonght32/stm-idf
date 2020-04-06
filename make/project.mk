a06c1dee6ee7cbc8c25af3ceabc2f02ab829b946
phonght32 <thanhphongho1998@gmail.com>
2020-04-06 19:41:46 +0700

    add c++ compile

--- make/project.mk
+++ make/project.mk
@@ -138,21 +138,26 @@ C_DEFS =  \
 ASFLAGS = $(MCU) $(AS_DEFS) $(INCLUDE_PATHS) $(OPT) -Wall -fdata-sections -ffunction-sections
 
 # C flags
-CFLAGS = $(MCU) $(C_DEFS) $(INCLUDE_PATHS) $(OPT) -Wall -fdata-sections -ffunction-sections
+CFLAGS = $(MCU) $(C_DEFS) $(INCLUDE_PATHS) $(OPT) -Wall -fdata-sections -ffunction-sections 
 ifeq ($(DEBUG), 1)
 CFLAGS += -g -gdwarf-2
 endif
-CFLAGS += -MMD -MP -MF"$(@:%.o=%.d)"
+CFLAGS += -MMD -MP -MF"$(@:%.o=%.d)" 
 
-# Get all source files include .c and .s
-C_SOURCES += $(foreach comp_src, $(SOURCE_PATHS), $(wildcard $(comp_src)/*.c))
-ASM_SOURCES += $(foreach comp_src, $(SOURCE_PATHS), $(wildcard $(comp_src)/*.s))
+# C++ flags
+CXXFLAGS = $(MCU) $(C_DEFS) $(INCLUDE_PATHS) $(OPT) -Wall -fdata-sections -ffunction-sections
+CXXFLAGS += -MMD -MP -MF"$(@:%.o=%.d)" 
 
-# link script
+# LD flags
 LDSCRIPT = stm-idf/make/stm32f4xx_flash.ld
 LIBS = -lc -lm -lnosys 
 LIBDIR = 
-LDFLAGS = $(MCU) -specs=nano.specs -T$(LDSCRIPT) $(LIBDIR) $(LIBS) -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref -Wl,--gc-sections
+LDFLAGS = $(MCU) -specs=nosys.specs -T$(LDSCRIPT) $(LIBDIR) $(LIBS) -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref -Wl,--gc-sections
+LDFLAGS += -fno-exceptions -fno-rtti 
+# Get all source files include .c and .s
+C_SOURCES += $(foreach comp_src, $(SOURCE_PATHS), $(wildcard $(comp_src)/*.c))
+CPP_SOURCES += $(foreach comp_src, $(SOURCE_PATHS), $(wildcard $(comp_src)/*.cpp))
+ASM_SOURCES += $(foreach comp_src, $(SOURCE_PATHS), $(wildcard $(comp_src)/*.s))
 
 # Target name
 TARGET = $(PROJECT_NAME)
@@ -163,6 +168,9 @@ all: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).hex $(BUILD_DIR)/$(TARGET
 # list of objects
 OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(C_SOURCES:.c=.o)))
 vpath %.c $(sort $(dir $(C_SOURCES)))
+# list of c++ program objects
+OBJECTS += $(addprefix $(BUILD_DIR)/,$(notdir $(CPP_SOURCES:.cpp=.o)))
+vpath %.cpp $(sort $(dir $(CPP_SOURCES)))
 # list of ASM program objects
 OBJECTS += $(addprefix $(BUILD_DIR)/,$(notdir $(ASM_SOURCES:.s=.o)))
 vpath %.s $(sort $(dir $(ASM_SOURCES)))
@@ -171,11 +179,14 @@ vpath %.s $(sort $(dir $(ASM_SOURCES)))
 $(BUILD_DIR)/%.o: %.c Makefile | $(BUILD_DIR) 
 	$(CC) -c $(CFLAGS) -Wa,-a,-ad,-alms=$(BUILD_DIR)/$(notdir $(<:.c=.lst)) $< -o $@
 
+$(BUILD_DIR)/%.o: %.cpp Makefile | $(BUILD_DIR) 
+	$(CXX) -c $(CXXFLAGS) -Wa,-a,-ad,-alms=$(BUILD_DIR)/$(notdir $(<:.cpp=.lst)) $< -o $@
+
 $(BUILD_DIR)/%.o: %.s Makefile | $(BUILD_DIR)
 	$(AS) -c $(CFLAGS) $< -o $@
 
 $(BUILD_DIR)/$(TARGET).elf: $(OBJECTS) Makefile
-	$(CC) $(OBJECTS) $(LDFLAGS) -o $@
+	$(CXX) $(OBJECTS) $(LDFLAGS) -o $@
 	$(SZ) $@
 
 $(BUILD_DIR)/%.hex: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
