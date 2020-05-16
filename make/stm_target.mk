f297875498053f4acea91ec2a9198d9ab447a31d
thanhphong98 <thanhphongho1998@gmail.com>
2020-05-16 08:32:36 +0700

    add makefile for private target

--- make/stm_target.mk
+++ make/stm_target.mk
@@ -1,48 +1,10 @@
 # Check STM32 product series
 ifeq ($(STM_SERIES), STM32F4) 
-STM_SERIES_PREFIX := stm32f4
-DEBUG = 1
-OPT = -Og
-CPU = -mcpu=cortex-m4
-FPU = -mfpu=fpv4-sp-d16
-FLOAT-ABI = -mfloat-abi=hard
-MCU = $(CPU) -mthumb $(FPU) $(FLOAT-ABI)
-AS_DEFS = 
-C_DEFS +=  \
--DUSE_HAL_DRIVER \
--DSTM32F407xx \
--DSTM32F4_TARGET 
+include $(STM_IDF_PATH)/make/stm32f4.mk
 endif
 
 ifeq ($(STM_SERIES), STM32F1) 
-STM_SERIES_PREFIX := stm32f1
-DEBUG = 1
-OPT = -Og
-CPU = -mcpu=cortex-m3
-MCU = $(CPU) -mthumb $(FPU) $(FLOAT-ABI)
-AS_DEFS = 
-C_DEFS +=  \
--DUSE_HAL_DRIVER \
--DSTM32F103xB \
--DSTM32F1_TARGET 
-endif
-
-# Check STM32 product line
-ifeq ($(STM_LINE), STM32F407) 
-STM_LINE_PREFIX := stm32f407
-endif
-
-ifeq ($(STM_LINE), STM32F103) 
-STM_LINE_PREFIX := stm32f103
-endif
-
-# Check STM32 target 
-ifeq ($(STM_TARGET), STM32F407VG) 
-STM_TARGET_PREFIX := stm32f407vg
-endif
-
-ifeq ($(STM_TARGET), STM32F103C8) 
-STM_TARGET_PREFIX := stm32f103c8
+include $(STM_IDF_PATH)/make/stm32f1.mk
 endif
 
 # Handle specify STM32 target.
